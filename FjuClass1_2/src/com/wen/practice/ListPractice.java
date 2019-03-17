0dcda34c3e648ba821ae571a79689a5abab89a76
Wendy Wu <wendy89711@gmail.com>
2019-03-17 23:52:50 +0800

    3/17-2

--- FjuClass1_2/src/com/wen/practice/ListPractice.java
+++ FjuClass1_2/src/com/wen/practice/ListPractice.java
@@ -8,18 +8,21 @@ public class ListPractice {
 
 	public static void main(String[] args) {
 		Random random = new Random();
+		int[] freq = new int[20];
 		List<Integer> list = new ArrayList<>();
 		for(int i=0; i<20; i++) {
-			list.add(0);
+			int r = random.nextInt(20);
+			list.add(r);
+			freq[r]++;
 		}
-		for(int i = 0;i<10000; i++) {
+/*		for(int i = 0;i<10000; i++) {
 			int r = random.nextInt(20);
 			Integer freq = list.get(r);
 			list.set(r, freq +1);
 			
-		}
-		for(int i=0;i<freq;i++) {
-			System.out.println(i+":"+list);
+		} */
+		for(int i=0;i<freq.length;i++) {
+			System.out.println(i+":"+freq[i]);
 		}
 		
 	}
