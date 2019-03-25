6c32d35cf082f2a7017f2f02ba30c42f61f5e2a0
wendy89711 <43363061+wendy89711@users.noreply.github.com>
2019-03-25 23:52:34 +0800

    Update Map.java
    3/25

--- FjuClass1_2/src/com/wen/practice/Map.java
+++ FjuClass1_2/src/com/wen/practice/Map.java
@@ -5,18 +5,20 @@ import java.util.Random;
 
 public class Map {
 
-	public static void main(String[] args) {
-		Random random = new Random(55);
+		public static void main(String[] args) {
+		Random random = new Random(50);
 		java.util.Map<Integer,Integer> n = new HashMap<>();
 		for(int i = 0; i < 10000; i++) {
 			int r = random.nextInt(20);
 			Integer freq = n.get(r);
-			if(freq == null) {
-				n.put(r, 1);
-			}else {
-				n.put(r, freq+1);
-			}
-		}System.out.println(n);
+			n.put(r, freq == null ? 1:freq +1);
+//			if(freq == null) {
+//				n.put(r, 1);
+//			}else {
+//				n.put(r, freq+1);
+//			}
+		}
+		System.out.println(n);
 	}
 
 }
