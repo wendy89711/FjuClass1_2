53a1dd57f50c46814fd91eb32207f86f917fe925
Wendy Wu <wendy89711@gmail.com>
2019-04-14 23:45:58 +0800

    4/14

--- FjuClass1_2/src/com/mar/M26.java
+++ FjuClass1_2/src/com/mar/M26.java
@@ -11,19 +11,18 @@ public class M26 {
 		Map<String, String> buy = new HashMap<>();
 		Scanner sca = new Scanner(System.in);
 		int i = 1;
-		String person;
 		String thing;
+		String amount;
 		while(i<=1) {
-			System.out.println("新增代購物:(輸入end清單輸入完畢)");
-			person = sca.nextLine();
-			if(person.contains("end")) {
-				System.out.println(buy);
-				sca.close();
-			}else {
-			System.out.println("新增代購物數量:");
+			System.out.println("新增代購物:(輸入q清單輸入完畢)");
 			thing = sca.nextLine();
-			buy.put(person, thing);
-			System.out.println(buy);
+			if(!"q".equals(thing)) {
+				System.out.println("新增代購物數量：");
+				amount = sca.nextLine();
+				buy.put(thing, amount);
+				System.out.println(buy);
+			}else {
+					break;
 			}
 		}
 		
