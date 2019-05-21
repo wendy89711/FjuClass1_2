5d76d19d7e20214b67cf59509ebb8895bd81d8bc
user <user@es301n21>
2019-05-21 12:02:15 +0800

    5/21 I/O

--- FjuClass1_2/src/com/may/BufferedInputFile.java
+++ FjuClass1_2/src/com/may/BufferedInputFile.java
@@ -1,31 +1,31 @@
 package com.may;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.Reader;
 
-public class May21 {
-
+public class BufferedInputFile {
 	public static void main(String[] args) {
 		try {
 			FileInputStream fis = new FileInputStream("may21.txt");
-			Reader in = new InputStreamReader(fis);
-			int n = in.read();
-			while(n!=-1) {
-				System.out.println(n);
-				n = in.read();
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader in = new BufferedReader(isr);
+			String line = in.readLine();
+			while(line != null) {
+				System.out.println(line);
+				line = in.readLine();
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (IOException e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-
 	}
-
+		
 }
