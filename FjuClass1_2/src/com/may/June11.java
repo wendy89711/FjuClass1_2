02f6b68a30378154067796d6dcf929087c4b03fb
Wendy Wu <wendy89711@gmail.com>
2019-06-13 22:22:05 +0800

    郵政地區[沒有]建構子版

--- FjuClass1_2/src/com/may/June11.java
+++ FjuClass1_2/src/com/may/June11.java
@@ -5,20 +5,25 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class June11 {
 
 	public static void main(String[] args) {
-		ArrayList<String> cities = new ArrayList<>();
-		ArrayList<String> areas = new ArrayList<>();
-		ArrayList<String> postID = new ArrayList<>();
+		List<String> cities = new ArrayList<>();
+		List<String> areas = new ArrayList<>();
+		List<String> postID = new ArrayList<>();
 		try {
 			BufferedReader in = new BufferedReader(new FileReader("areas.txt"));
 			String line = in.readLine();
+			String lastCity = "";
 			while(line != null) {
 				System.out.println(line);
 				String[] tokens = line.split(",");
-				cities.add(tokens[0]);
+				if(!tokens[0].equals(lastCity)) {
+					lastCity = tokens[0];
+					cities.add(lastCity);
+				}
 				areas.add(tokens[1]);
 				postID.add(tokens[2]);
 				line = in.readLine();
@@ -26,6 +31,8 @@ public class June11 {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			e.printStackTrace();
+		} catch(ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
 		System.out.println(cities);
